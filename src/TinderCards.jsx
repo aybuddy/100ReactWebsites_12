4b3e642eb69e8affcf07835f951176d37862b455
Aaron Yu <ayybuddyy@gmail.com>
2021-03-03 21:42:22 -0500

    Connected TinderCard component to firebase

--- src/TinderCards.jsx
+++ src/TinderCards.jsx
@@ -1,20 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import TinderCard from 'react-tinder-card';
+import database from './firebase';
 import './TinderCards.css';
 
 function TinderCards() {
-  const [people, setPeople] = useState([
-    {
-      name: 'steve jobs',
-      url:
-        'https://thumbor.forbes.com/thumbor/fit-in/416x416/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5b8576db31358e0429c734e3%2F0x0.jpg%3Fbackground%3D000000%26cropX1%3D211%26cropX2%3D2381%26cropY1%3D900%26cropY2%3D3072',
-    },
-    {
-      name: 'elon musk',
-      url:
-        'https://upload.wikimedia.org/wikipedia/commons/8/85/Elon_Musk_Royal_Society_%28crop1%29.jpg',
-    },
-  ]);
+  const [people, setPeople] = useState([]);
+
+  useEffect(() => {
+    // where code runs
+    database
+      .collection('people')
+      .onSnapshot((snapshot) =>
+        setPeople(snapshot.docs.map((doc) => doc.data()))
+      );
+  }, []);
 
   // BAD way, standard JS way
   // const people = []
--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
-import firebase from './firebase';
+import firebase from 'firebase';
 
-const firebaseConfig = {
+const firebaseApp = firebase.initializeApp({
   apiKey: 'AIzaSyDw-zEJ0KvJs3PqXgwb8ZEtrAKnSK044Hw',
   authDomain: 'tinder-clone-cp-4e6f2.firebaseapp.com',
   projectId: 'tinder-clone-cp-4e6f2',
@@ -8,4 +8,11 @@ const firebaseConfig = {
   messagingSenderId: '9774582359',
   appId: '1:9774582359:web:2bec307b10b2c80c61f112',
   measurementId: 'G-C8R22TBYLQ',
-};
+});
+
+// const firebaseApp = firebase.initializeApp(firebaseConfig);
+// const database = firebaseApp.firestore();
+
+const database = firebaseApp.firestore();
+
+export default database;
