41d99bd3fde2935f047a43a950464b774bf0d06a
Aaron Yu <ayybuddyy@gmail.com>
2021-03-03 20:12:39 -0500

    Clean up files

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+import firebase from './firebase';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDw-zEJ0KvJs3PqXgwb8ZEtrAKnSK044Hw',
+  authDomain: 'tinder-clone-cp-4e6f2.firebaseapp.com',
+  projectId: 'tinder-clone-cp-4e6f2',
+  storageBucket: 'tinder-clone-cp-4e6f2.appspot.com',
+  messagingSenderId: '9774582359',
+  appId: '1:9774582359:web:2bec307b10b2c80c61f112',
+  measurementId: 'G-C8R22TBYLQ',
+};
