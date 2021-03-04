d29a2601daaefadfd100b8ba3a84cb6ff2cb82fc
Aaron Yu <ayybuddyy@gmail.com>
2021-03-03 20:47:25 -0500

    Add react-router-dom and set up navigation in app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className='App'>
       <Header />
-      {/* Tinder cards */}
-      {/* buttons below cards */}
+      <Router>
+        <Switch>
+          <Route path='/chat'>
+            <h1>I am the chat page</h1>
+          </Route>
+          <Route path='/'>
+            <h1>I am the home page</h1>
+          </Route>
+        </Switch>
+        {/* Tinder cards */}
+        {/* buttons below cards */}
 
-      {/* chats screen */}
-      {/* individual chat screen */}
+        {/* chats screen */}
+        {/* individual chat screen */}
+      </Router>
     </div>
   );
 }
